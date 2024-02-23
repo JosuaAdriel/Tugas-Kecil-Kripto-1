deb3765a3f76f35acab41c9991e661778f721353
ceavinrufus <18221162@std.stei.itb.ac.id>
2024-02-24 00:43:07 +0700

    update playfair

--- src/utils/playfair-cipher.js
+++ src/utils/playfair-cipher.js
@@ -1,114 +1,136 @@
-// Fungsi untuk mengonversi teks pesan menjadi bigram
-function generateBigrams(message) {
-  // Menghapus karakter non-alfanumerik dan mengonversi ke huruf kecil
-  message = message.toLowerCase().replace(/[^a-z]/g, "");
+function PlayfairCipher(keyword) {
+  const size = 5;
+  let playfairMatrix = [];
+  let keywordProcessed = processKeyword(keyword);
 
-  // Memisahkan teks menjadi bigram
-  let bigrams = [];
-  for (let i = 0; i < message.length; i += 2) {
-    let pair = message[i];
-    if (i + 1 < message.length) {
-      if (message[i] === message[i + 1]) {
-        pair += "x"; // Menambahkan x jika ada dua huruf yang sama bersebelahan
-        i--; // Mengulang iterasi untuk menangani huruf kedua pada pasangan
-      } else {
-        pair += message[i + 1];
+  function processKeyword(keyword) {
+    let processedKeyword = "";
+    let keywordSet = new Set();
+    keyword = keyword.replace(/j/g, "i").replace(/\s/g, "");
+
+    for (let char of keyword) {
+      if (!keywordSet.has(char)) {
+        keywordSet.add(char);
+        processedKeyword += char;
       }
+    }
+
+    let alphabet = "abcdefghiklmnopqrstuvwxyz";
+    for (let char of alphabet) {
+      if (!keywordSet.has(char)) {
+        processedKeyword += char;
+      }
+    }
+
+    return processedKeyword;
+  }
+
+  function generateMatrix() {
+    for (let i = 0; i < size; i++) {
+      playfairMatrix.push([]);
+      for (let j = 0; j < size; j++) {
+        playfairMatrix[i].push(keywordProcessed.charAt(i * size + j));
+      }
+    }
+  }
+
+  function findCoordinates(char) {
+    let coordinates = { x: -1, y: -1 };
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        if (playfairMatrix[i][j] === char) {
+          coordinates.x = i;
+          coordinates.y = j;
+          return coordinates;
+        }
+      }
+    }
+    return coordinates;
+  }
+
+  function encryptDigram(digram) {
+    let coord1 = findCoordinates(digram.charAt(0));
+    let coord2 = findCoordinates(digram.charAt(1));
+    let encryptedDigram = "";
+
+    if (coord1.x === coord2.x) {
+      encryptedDigram += playfairMatrix[coord1.x][(coord1.y + 1) % size];
+      encryptedDigram += playfairMatrix[coord2.x][(coord2.y + 1) % size];
+    } else if (coord1.y === coord2.y) {
+      encryptedDigram += playfairMatrix[(coord1.x + 1) % size][coord1.y];
+      encryptedDigram += playfairMatrix[(coord2.x + 1) % size][coord2.y];
     } else {
-      pair += "x"; // Menambahkan x jika panjang pesan ganjil
+      encryptedDigram += playfairMatrix[coord1.x][coord2.y];
+      encryptedDigram += playfairMatrix[coord2.x][coord1.y];
     }
-    bigrams.push(pair);
-  }
-  return bigrams;
-}
 
-// Fungsi untuk menghasilkan matriks kunci
-function generateKeyMatrix(key) {
-  let alphabet = "abcdefghiklmnopqrstuvwxyz"; // Menghilangkan huruf j
-  let keyMatrix = [];
-  let keyWithoutDuplicates = "";
-
-  // Membuang karakter duplikat dari kunci
-  for (let i = 0; i < key.length; i++) {
-    if (!keyWithoutDuplicates.includes(key[i])) {
-      keyWithoutDuplicates += key[i];
-    }
+    return encryptedDigram;
   }
 
-  // Mengisi matriks kunci
-  let index = 0;
-  for (let i = 0; i < 5; i++) {
-    let row = [];
-    for (let j = 0; j < 5; j++) {
-      if (index < keyWithoutDuplicates.length) {
-        row.push(keyWithoutDuplicates[index]);
-        alphabet = alphabet.replace(keyWithoutDuplicates[index], ""); // Menghapus huruf yang sudah digunakan dari alfabet
-        index++;
-      } else {
-        row.push(alphabet[0]);
-        alphabet = alphabet.substring(1);
-      }
-    }
-    keyMatrix.push(row);
-  }
-  return keyMatrix;
-}
+  function decryptDigram(digram) {
+    let coord1 = findCoordinates(digram.charAt(0));
+    let coord2 = findCoordinates(digram.charAt(1));
+    let decryptedDigram = "";
 
-// Fungsi untuk mendapatkan posisi huruf dalam matriks kunci
-function getPosition(matrix, letter) {
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      if (matrix[i][j] === letter) {
-        return { row: i, col: j };
-      }
-    }
-  }
-}
-
-// Fungsi untuk mengenkripsi pesan menggunakan algoritma Playfair Cipher
-function encrypt(plaintext, key) {
-  let bigrams = generateBigrams(plaintext);
-  let keyMatrix = generateKeyMatrix(key);
-  let ciphertext = "";
-
-  for (let i = 0; i < bigrams.length; i++) {
-    let firstLetter = bigrams[i][0];
-    let secondLetter = bigrams[i][1];
-
-    let firstPosition = getPosition(keyMatrix, firstLetter);
-    let secondPosition = getPosition(keyMatrix, secondLetter);
-
-    let encryptedBigram = "";
-    if (firstPosition.row === secondPosition.row) {
-      // Jika dalam baris yang sama
-      encryptedBigram +=
-        keyMatrix[firstPosition.row][(firstPosition.col + 1) % 5];
-      encryptedBigram +=
-        keyMatrix[secondPosition.row][(secondPosition.col + 1) % 5];
-    } else if (firstPosition.col === secondPosition.col) {
-      // Jika dalam kolom yang sama
-      encryptedBigram +=
-        keyMatrix[(firstPosition.row + 1) % 5][firstPosition.col];
-      encryptedBigram +=
-        keyMatrix[(secondPosition.row + 1) % 5][secondPosition.col];
+    if (coord1.x === coord2.x) {
+      decryptedDigram += playfairMatrix[coord1.x][(coord1.y + size - 1) % size];
+      decryptedDigram += playfairMatrix[coord2.x][(coord2.y + size - 1) % size];
+    } else if (coord1.y === coord2.y) {
+      decryptedDigram += playfairMatrix[(coord1.x + size - 1) % size][coord1.y];
+      decryptedDigram += playfairMatrix[(coord2.x + size - 1) % size][coord2.y];
     } else {
-      // Jika berada dalam baris dan kolom yang berbeda
-      encryptedBigram += keyMatrix[firstPosition.row][secondPosition.col];
-      encryptedBigram += keyMatrix[secondPosition.row][firstPosition.col];
+      decryptedDigram += playfairMatrix[coord1.x][coord2.y];
+      decryptedDigram += playfairMatrix[coord2.x][coord1.y];
     }
 
-    ciphertext += encryptedBigram;
+    return decryptedDigram;
   }
 
-  return ciphertext;
+  this.encrypt = function (plaintext) {
+    let processedPlaintext = plaintext
+      .replace(/j/g, "i")
+      .replace(/\s/g, "")
+      .toLowerCase();
+    let encryptedText = "";
+    generateMatrix();
+
+    for (let i = 0; i < processedPlaintext.length; i += 2) {
+      let digram = processedPlaintext.charAt(i);
+      if (
+        i + 1 < processedPlaintext.length &&
+        processedPlaintext.charAt(i) === processedPlaintext.charAt(i + 1)
+      ) {
+        digram += "x";
+        i--;
+      } else if (i + 1 < processedPlaintext.length) {
+        digram += processedPlaintext.charAt(i + 1);
+      } else {
+        digram += "x";
+      }
+      encryptedText += encryptDigram(digram);
+    }
+
+    return encryptedText.toUpperCase();
+  };
+
+  this.decrypt = function (encryptedText) {
+    let processedEncryptedText = encryptedText.toLowerCase();
+    let decryptedText = "";
+    generateMatrix();
+
+    for (let i = 0; i < processedEncryptedText.length; i += 2) {
+      let digram = processedEncryptedText.substr(i, 2);
+      decryptedText += decryptDigram(digram);
+    }
+
+    return decryptedText.toUpperCase();
+  };
 }
 
-// Contoh penggunaan
-let plaintext = "hello";
-let key = "keyword";
-
-console.log("Plaintext:", plaintext);
-console.log("Key:", key);
-
-let encryptedText = encrypt(plaintext, key);
-console.log("Encrypted Text:", encryptedText);
+// Example usage
+let keyword = "jalan ganesha sepuluh";
+let cipher = new PlayfairCipher(keyword);
+let plaintext = "temui ibu nanti malam";
+let encryptedText = cipher.encrypt(plaintext);
+console.log("Encrypted:", encryptedText);
+console.log("Decrypted:", cipher.decrypt(encryptedText));
