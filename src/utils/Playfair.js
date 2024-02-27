2d9b014ac1ff090c0575cf9f9e8d37dd6feeeb66
ceavinrufus <18221162@std.stei.itb.ac.id>
2024-02-27 08:52:35 +0700

    fix playfair

--- src/utils/Playfair.js
+++ src/utils/Playfair.js
@@ -1,142 +1,121 @@
 class Playfair {
-  constructor(keyword) {
-    this.keyword = keyword;
-    this.size = 5;
-    this.playfairMatrix = [];
-    this.keywordProcessed = this.processKeyword(keyword);
+  constructor(key) {
+    this.key = this.prepareKey(key);
+    this.keySquare = this.generateKeySquare(this.key);
   }
 
-  processKeyword(keyword) {
-    let processedKeyword = "";
-    let keywordSet = new Set();
-    keyword = keyword.replace(/j/g, "i").replace(/\s/g, "");
-
-    for (let char of keyword) {
-      if (!keywordSet.has(char)) {
-        keywordSet.add(char);
-        processedKeyword += char;
+  prepareKey(key) {
+    // Remove spaces and duplicate letters
+    key = key.replace(/ /g, "").toUpperCase();
+    let uniqueChars = [];
+    for (let char of key) {
+      if (!uniqueChars.includes(char)) {
+        uniqueChars.push(char);
       }
     }
-
-    let alphabet = "abcdefghiklmnopqrstuvwxyz";
-    for (let char of alphabet) {
-      if (!keywordSet.has(char)) {
-        processedKeyword += char;
-      }
-    }
-
-    return processedKeyword;
+    return uniqueChars.join("");
   }
 
-  generateMatrix() {
-    for (let i = 0; i < this.size; i++) {
-      this.playfairMatrix.push([]);
-      for (let j = 0; j < this.size; j++) {
-        this.playfairMatrix[i].push(
-          this.keywordProcessed.charAt(i * this.size + j)
-        );
-      }
-    }
-  }
+  generateKeySquare(key) {
+    let keySquare = [];
+    let alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"; // Excluding 'J'
+    let keyIndex = 0;
 
-  findCoordinates(char) {
-    let coordinates = { x: -1, y: -1 };
-    for (let i = 0; i < this.size; i++) {
-      for (let j = 0; j < this.size; j++) {
-        if (this.playfairMatrix[i][j] === char) {
-          coordinates.x = i;
-          coordinates.y = j;
-          return coordinates;
+    // Populate key-related characters first
+    for (let i = 0; i < 5; i++) {
+      let row = [];
+      for (let j = 0; j < 5; j++) {
+        if (keyIndex < key.length) {
+          row.push(key[keyIndex]);
+          keyIndex++;
+        } else {
+          // Fill the rest of the grid with remaining alphabet letters
+          while (key.includes(alphabet[0])) {
+            alphabet = alphabet.slice(1);
+          }
+          row.push(alphabet[0]);
+          alphabet = alphabet.slice(1);
         }
       }
+      keySquare.push(row);
     }
-    return coordinates;
-  }
-
-  encryptDigram(digram) {
-    let coord1 = this.findCoordinates(digram.charAt(0));
-    let coord2 = this.findCoordinates(digram.charAt(1));
-    let encryptedDigram = "";
-
-    if (coord1.x === coord2.x) {
-      encryptedDigram +=
-        this.playfairMatrix[coord1.x][(coord1.y + 1) % this.size];
-      encryptedDigram +=
-        this.playfairMatrix[coord2.x][(coord2.y + 1) % this.size];
-    } else if (coord1.y === coord2.y) {
-      encryptedDigram +=
-        this.playfairMatrix[(coord1.x + 1) % this.size][coord1.y];
-      encryptedDigram +=
-        this.playfairMatrix[(coord2.x + 1) % this.size][coord2.y];
-    } else {
-      encryptedDigram += this.playfairMatrix[coord1.x][coord2.y];
-      encryptedDigram += this.playfairMatrix[coord2.x][coord1.y];
-    }
-
-    return encryptedDigram;
-  }
-
-  decryptDigram(digram) {
-    let coord1 = this.findCoordinates(digram.charAt(0));
-    let coord2 = this.findCoordinates(digram.charAt(1));
-    let decryptedDigram = "";
-
-    if (coord1.x === coord2.x) {
-      decryptedDigram +=
-        this.playfairMatrix[coord1.x][(coord1.y + this.size - 1) % this.size];
-      decryptedDigram +=
-        this.playfairMatrix[coord2.x][(coord2.y + this.size - 1) % this.size];
-    } else if (coord1.y === coord2.y) {
-      decryptedDigram +=
-        this.playfairMatrix[(coord1.x + this.size - 1) % this.size][coord1.y];
-      decryptedDigram +=
-        this.playfairMatrix[(coord2.x + this.size - 1) % this.size][coord2.y];
-    } else {
-      decryptedDigram += this.playfairMatrix[coord1.x][coord2.y];
-      decryptedDigram += this.playfairMatrix[coord2.x][coord1.y];
-    }
-
-    return decryptedDigram;
+    return keySquare;
   }
 
   encrypt(plaintext) {
-    let processedPlaintext = plaintext
-      .replace(/j/g, "i")
-      .replace(/\s/g, "")
-      .toLowerCase();
-    let encryptedText = "";
-    this.generateMatrix();
+    plaintext = plaintext.toUpperCase().replace(/J/g, "I").replace(/ /g, "");
+    let ciphertext = "";
+    for (let i = 0; i < plaintext.length; i += 2) {
+      let char1 = plaintext[i];
+      let char2 = plaintext[i + 1] ? plaintext[i + 1] : "X"; // Add 'X' if the last digraph has only one character
+      let pair1 = this.findCharPosition(char1);
+      let pair2 = this.findCharPosition(char2);
 
-    for (let i = 0; i < processedPlaintext.length; i += 2) {
-      let digram = processedPlaintext.charAt(i);
-      if (
-        i + 1 < processedPlaintext.length &&
-        processedPlaintext.charAt(i) === processedPlaintext.charAt(i + 1)
-      ) {
-        digram += "x";
-        i--;
-      } else if (i + 1 < processedPlaintext.length) {
-        digram += processedPlaintext.charAt(i + 1);
-      } else {
-        digram += "x";
-      }
-      encryptedText += this.encryptDigram(digram);
+      let encryptedPair = this.encryptPair(pair1, pair2);
+      ciphertext += encryptedPair[0] + encryptedPair[1];
     }
-
-    return encryptedText.toUpperCase();
+    return ciphertext;
   }
 
-  decrypt(encryptedText) {
-    let processedEncryptedText = encryptedText.toLowerCase();
-    let decryptedText = "";
-    this.generateMatrix();
+  decrypt(ciphertext) {
+    let plaintext = "";
+    for (let i = 0; i < ciphertext.length; i += 2) {
+      let char1 = ciphertext[i];
+      let char2 = ciphertext[i + 1];
+      let pair1 = this.findCharPosition(char1);
+      let pair2 = this.findCharPosition(char2);
 
-    for (let i = 0; i < processedEncryptedText.length; i += 2) {
-      let digram = processedEncryptedText.substr(i, 2);
-      decryptedText += this.decryptDigram(digram);
+      let decryptedPair = this.decryptPair(pair1, pair2);
+      plaintext += decryptedPair[0] + decryptedPair[1];
+    }
+    return plaintext;
+  }
+
+  findCharPosition(char) {
+    for (let i = 0; i < this.keySquare.length; i++) {
+      let row = this.keySquare[i];
+      if (row.includes(char)) {
+        return [i, row.indexOf(char)];
+      }
+    }
+  }
+
+  encryptPair(pair1, pair2) {
+    let [row1, col1] = pair1;
+    let [row2, col2] = pair2;
+
+    if (row1 === row2) {
+      col1 = (col1 + 1) % 5;
+      col2 = (col2 + 1) % 5;
+    } else if (col1 === col2) {
+      row1 = (row1 + 1) % 5;
+      row2 = (row2 + 1) % 5;
+    } else {
+      let temp = col1;
+      col1 = col2;
+      col2 = temp;
     }
 
-    return decryptedText.toUpperCase();
+    return [this.keySquare[row1][col1], this.keySquare[row2][col2]];
+  }
+
+  decryptPair(pair1, pair2) {
+    let [row1, col1] = pair1;
+    let [row2, col2] = pair2;
+
+    if (row1 === row2) {
+      col1 = (col1 - 1 + 5) % 5;
+      col2 = (col2 - 1 + 5) % 5;
+    } else if (col1 === col2) {
+      row1 = (row1 - 1 + 5) % 5;
+      row2 = (row2 - 1 + 5) % 5;
+    } else {
+      let temp = col1;
+      col1 = col2;
+      col2 = temp;
+    }
+
+    return [this.keySquare[row1][col1], this.keySquare[row2][col2]];
   }
 }
 
