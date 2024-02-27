b0e83452e08796abed94e5e883f8b3d24da915c0
ceavinrufus <18221162@std.stei.itb.ac.id>
2024-02-28 01:10:25 +0700

    autokey

---
+++ src/utils/AutokeyVigenereCipher.js
@@ -0,0 +1,54 @@
+class AutokeyVigenereCipher {
+  constructor(keyword) {
+    this.keyword = keyword.toUpperCase();
+  }
+
+  encrypt(plaintext) {
+    plaintext = plaintext.toUpperCase().replace(/[^A-Z]/g, "");
+    let key = this.generateKey(plaintext);
+    let ciphertext = "";
+    for (let i = 0; i < plaintext.length; i++) {
+      const plainCharCode = plaintext.charCodeAt(i);
+      const keyCharCode = key.charCodeAt(i);
+      const encryptedCharCode = ((plainCharCode + keyCharCode) % 26) + 65;
+      ciphertext += String.fromCharCode(encryptedCharCode);
+    }
+    return ciphertext;
+  }
+
+  decrypt(ciphertext) {
+    let key = this.keyword;
+    ciphertext = ciphertext.toUpperCase().replace(/[^A-Z]/g, "");
+
+    let plaintext = "";
+    for (let i = 0; i < ciphertext.length; i++) {
+      const cipherCharCode = ciphertext.charCodeAt(i);
+      const keyCharCode = key.charCodeAt(i) - 65;
+      const decryptedCharCode =
+        ((cipherCharCode - 65 - keyCharCode + 26) % 26) + 65;
+      plaintext += String.fromCharCode(decryptedCharCode);
+      key += String.fromCharCode(decryptedCharCode);
+    }
+    return plaintext;
+  }
+
+  generateKey(plaintext) {
+    let key = this.keyword;
+    plaintext = plaintext.toUpperCase().replace(/[^A-Z]/g, "");
+
+    let diff = plaintext.length - key.length;
+    if (diff > 0) {
+      key += plaintext.slice(0, diff);
+    }
+
+    return key;
+  }
+}
+
+// Example usage:
+const keyword = "INDO";
+const cipher = new AutokeyVigenereCipher(keyword);
+const plaintext = "negarapenghasilminyakmentahdidunia";
+const encryptedText = cipher.encrypt(plaintext);
+console.log("Encrypted:", encryptedText);
+console.log("Decrypted:", cipher.decrypt(encryptedText));
